81f69d899f1da1e7e19b55cdd8d9e74dc02b1c4f
Eric Smith <ericsmithalan@gmail.com>
2020-05-10 20:46:09 -0400

    updates

--- packages/email-client/src/shared/components/App/index.tsx
+++ packages/email-client/src/shared/components/App/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
+import { Div } from "@email/element/src";
 
 function App(): JSX.Element {
-    return <div>hello2sadfsdf</div>;
+    return <Div>hello</Div>;
 }
 
 export default hot(App);
--- packages/email-element/src/index.ts
+++ packages/email-element/src/index.ts
@@ -1,15 +1,15 @@
-export { A, IAElement } from "./primitives/A";
-export { Body, IBodyElement } from "./primitives/Body";
-export { Div, IDivElement } from "./primitives/Div";
-export { Hr, IHrElement } from "./primitives/Hr";
-export { Img, IImgElement } from "./primitives/Img";
-export { Li, ILilElement } from "./primitives/Li";
-export { Ol, IOlElement } from "./primitives/Ol";
-export { Span, ISpanElement } from "./primitives/Span";
-export { Table, ITableElement } from "./primitives/Table";
-export { Td, ITdElement } from "./primitives/Td";
-export { Tr, ITrElement } from "./primitives/Tr";
-export { Ul, IUlElement } from "./primitives/Ul";
+export { A } from "./primitives/A";
+export { Body } from "./primitives/Body";
+export { Div } from "./primitives/Div";
+export { Hr } from "./primitives/Hr";
+export { Img } from "./primitives/Img";
+export { Li } from "./primitives/Li";
+export { Ol } from "./primitives/Ol";
+export { Span } from "./primitives/Span";
+export { Table } from "./primitives/Table";
+export { Td } from "./primitives/Td";
+export { Tr } from "./primitives/Tr";
+export { Ul } from "./primitives/Ul";
 export { Label } from "./Label";
 export { ThreeColLayout, IThreeColLayout } from "./layouts/ThreeCol";
 
